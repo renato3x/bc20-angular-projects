63528a73ee160d4850caa0260a7833b6e6ed8c36
renato3x <pereirarenato21@gmail.com>
2022-06-21 11:59:32 -0300

    reactive-forms 2

--- forms/src/app/reactive-forms/reactive-forms.component.ts
+++ forms/src/app/reactive-forms/reactive-forms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-forms',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReactiveFormsComponent implements OnInit {
 
+  // controlNome: FormControl = new FormControl('')
+  formUsuario: FormGroup = new FormGroup({
+    nome: new FormControl('', [ Validators.required ]),
+    username: new FormControl('', [ Validators.required, Validators.minLength(8) ]),
+    email: new FormControl('', [ Validators.required, Validators.email ]),
+    senha: new FormControl('', [ Validators.required, Validators.minLength(8) ]),
+    genero: new FormControl(''),
+    termos: new FormControl(true, [ Validators.requiredTrue ])
+  })
+
+  /**
+   * O requiredTrue é utilizado para validar se o campo está marcado ou não
+   */
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  enviar(): void {
+    console.log(this.formUsuario.value) // retorna apenas os valores digitados nos campos
+    console.log(this.formUsuario.controls) // retorna todos os dados dos campos
+
+    // this.formUsuario.reset() // reseta todos os campos
+  }
 }
